a4b5972135935d1581e176b429d45178ab11dcab
Dom <domkav@gmail.com>
2023-07-01 16:54:39 +0100

    Delete hookproc.cpp

--- hookproc.cpp
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-#include "pch.h"
-#include "hookproc.h"
-
-LRESULT CALLBACK YourHookProcedure(int nCode, WPARAM wParam, LPARAM lParam) {
-    // Handle the hooked events here
-    return CallNextHookEx(NULL, nCode, wParam, lParam);
-}
