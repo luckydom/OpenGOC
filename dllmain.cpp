418e7ca697fa616f475761d9f0cf64c20ae71227
Dom <domkav@gmail.com>
2023-07-11 21:56:40 +0100

    working function hooking

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,24 +1,77 @@
 #include "pch.h"
 #include <Windows.h>
+#include <iostream>
+#include <vector>
 
-DWORD WINAPI MainThread(LPVOID param) {
-	while (true) {
-		if (GetAsyncKeyState(VK_F6) & 0x80000) {
-			MessageBoxA(NULL, "F6 pressed!", "F6 pressed!", MB_OK);
-		}
-		Sleep(100);
-	}
-	return 0;
+#define WRITE_ADDRESS_STRICTALIAS(data, addr) \
+* (data + 0) = ((addr) & 0x000000ff) >> 0;   \
+* (data + 1) = ((addr) & 0x0000ff00) >> 8;   \
+* (data + 2) = ((addr) & 0x00ff0000) >> 16;  \
+* (data + 3) = ((addr) & 0xff000000) >> 24;
+
+// Original function signature
+typedef int(__stdcall* WinMainFunction)(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
+WinMainFunction originalWinMainFunc = nullptr;
+
+// Custom function that will be called instead of the original function
+// int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+int __stdcall customFunction(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    OutputDebugStringA("DLL: I AM HERE");
+    originalWinMainFunc = reinterpret_cast<WinMainFunction>(0x0069EDC0);
+    return originalWinMainFunc(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
 }
-bool WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, LPVOID lpReserved) {
-	if (dwReason == DLL_PROCESS_ATTACH) {
-		//MessageBoxA(NULL, "DLL injected!", "DLL injectuotas!", MB_OK);
-		OutputDebugStringA("DLL: injected!!");
-		CreateThread(0, 0, MainThread, hModule, 0, 0);
-	}
-	if (dwReason == DLL_PROCESS_DETACH) {
-		MessageBoxA(NULL, "DLL de!", "DLL detached!", MB_OK);
-		CreateThread(0, 0, MainThread, hModule, 0, 0);
-	}
-	return true;
+
+void writeMemory(uint32_t address, const void* data, size_t size)
+{
+#ifdef _WIN32
+    if (!WriteProcessMemory(GetCurrentProcess(), (LPVOID)address, data, size, nullptr))
+    {
+        throw std::runtime_error("WriteProcessMemory failed");
+    }
+#else
+    // We own the pages with PROT_WRITE | PROT_EXEC, we can simply just memcpy the data
+    std::memcpy((void*)address, data, size);
+#endif // _WIN32
+}
+
+void writeJmp(uint32_t address, void* fn)
+{
+    uint8_t data[5] = { 0 };
+    data[0] = 0xE9; // JMP
+
+    auto addr = reinterpret_cast<uintptr_t>(fn);
+    WRITE_ADDRESS_STRICTALIAS(&data[1], addr - address - 5);
+
+    writeMemory(address, data, sizeof(data));
+}
+
+void doThings() {
+    char buffer[100];
+    sprintf_s(buffer, "func mem addr is %p ", &customFunction);
+    OutputDebugStringA(buffer);
+
+    writeJmp(0x00406195, customFunction); // jmp WinMain
+    OutputDebugStringA("DLL: AFTER");
+
+    //std::vector<uint8_t> opCodes = {0xC2, 0x10, 0x00};
+    //writeMemory(0x0069EDC0, opCodes.data(), opCodes.size());
+}
+
+// Entry point of the DLL
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        // Place where we obtain the memory address of the original function
+        doThings();
+        break;
+
+    case DLL_PROCESS_DETACH:
+        OutputDebugStringA("DLL: DETACHED!");
+        break;
+    }
+
+    return TRUE;
 }
