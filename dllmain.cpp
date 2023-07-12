354a2b0e1aa492ca922aea43267441d3bfbb264a
Dom <domkav@gmail.com>
2023-07-12 11:17:58 +0100

    clean up

--- dllmain.cpp
+++ dllmain.cpp
@@ -9,15 +9,22 @@
 * (data + 2) = ((addr) & 0x00ff0000) >> 16;  \
 * (data + 3) = ((addr) & 0xff000000) >> 24;
 
+__declspec(naked)  int __fastcall native_sub_405966(HINSTANCE hInstance, int nCmdShow)
+{
+    __asm {
+        push 00405966h
+        ret
+    }
+}
+
 // Original function signature
 typedef int(__stdcall* WinMainFunction)(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
 WinMainFunction originalWinMainFunc = nullptr;
 
 // Custom function that will be called instead of the original function
-// int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
-int __stdcall customFunction(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+int __stdcall myWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-    OutputDebugStringA("DLL: I AM HERE");
+    OutputDebugStringA("DLL: myWinMain() func is running");
     originalWinMainFunc = reinterpret_cast<WinMainFunction>(0x0069EDC0);
     return originalWinMainFunc(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
 }
@@ -46,14 +53,13 @@ void writeJmp(uint32_t address, void* fn)
     writeMemory(address, data, sizeof(data));
 }
 
-void doThings() {
-    char buffer[100];
-    sprintf_s(buffer, "func mem addr is %p ", &customFunction);
-    OutputDebugStringA(buffer);
+void HookFunctions() {
+    writeJmp(0x00406195, myWinMain); // jmp WinMain
+    OutputDebugStringA("DLL: All functions hooked");
 
-    writeJmp(0x00406195, customFunction); // jmp WinMain
-    OutputDebugStringA("DLL: AFTER");
-
+    //char buffer[100];
+    //sprintf_s(buffer, "func mem addr is %p ", &myWinMain);
+    //OutputDebugStringA(buffer);
     //std::vector<uint8_t> opCodes = {0xC2, 0x10, 0x00};
     //writeMemory(0x0069EDC0, opCodes.data(), opCodes.size());
 }
@@ -63,14 +69,14 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 {
     switch (ul_reason_for_call)
     {
-    case DLL_PROCESS_ATTACH:
-        // Place where we obtain the memory address of the original function
-        doThings();
-        break;
+        case DLL_PROCESS_ATTACH:
+            OutputDebugStringA("DLL: Attached");
+            HookFunctions();
+            break;
 
-    case DLL_PROCESS_DETACH:
-        OutputDebugStringA("DLL: DETACHED!");
-        break;
+        case DLL_PROCESS_DETACH:
+            OutputDebugStringA("DLL: Detached");
+            break;
     }
 
     return TRUE;
