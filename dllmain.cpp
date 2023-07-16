db44337e796228954168b94bf6870f1af15934a3
Dom <domkav@gmail.com>
2023-07-16 03:28:58 +0100

    gained control of DrawMenuItemsFunc

--- dllmain.cpp
+++ dllmain.cpp
@@ -56,7 +56,7 @@ Sub_69F0D0 originalFirstRunFunc = nullptr;
 
 HWND __fastcall local_sub_405966(HINSTANCE hInstance, int nCmdShow) // 405966 is jmp
 {
-    OutputDebugStringA("DLL: 1st -> local_sub_405966()");
+    //OutputDebugStringA("DLL: 1st -> local_sub_405966()");
     originalFirstRunFunc = reinterpret_cast<Sub_69F0D0>(0x0069F0D0);
     return originalFirstRunFunc(hInstance, nCmdShow);
 }
@@ -75,7 +75,7 @@ Sub_72CAB0 originalSecondRunFunc = nullptr;
 
 BOOL __fastcall local_sub_4013B6(int pThis) // 4013B6 is jmp
 {
-    OutputDebugStringA("DLL: 2nd -> local_sub_4013B6()");
+    //OutputDebugStringA("DLL: 2nd -> local_sub_4013B6()");
     originalSecondRunFunc = reinterpret_cast<Sub_72CAB0>(0x0072CAB0);
     return originalSecondRunFunc((void *)pThis);
 }
@@ -94,7 +94,7 @@ Sub_7653F0 originalThirdRunFunc = nullptr;
 
 char __fastcall local_sub_403D05(uint32_t* pThis, int a1)
 {
-    OutputDebugStringA("DLL: 3rd -> local_sub_403D05()");
+    //OutputDebugStringA("DLL: 3rd -> local_sub_403D05()");
     originalThirdRunFunc = reinterpret_cast<Sub_7653F0>(0x007653F0);
     return originalThirdRunFunc(pThis, a1);
 }
@@ -116,7 +116,7 @@ Sub_729700 *originalFourthRunFunc = nullptr;
 
 CMFCRibbonBar *local_sub_4017C6()
 {
-    OutputDebugStringA("DLL: 4th -> local_sub_4017C6()");
+    //OutputDebugStringA("DLL: 4th -> local_sub_4017C6()");
     originalFourthRunFunc = reinterpret_cast<Sub_729700*>(0x00729700);
     return originalFourthRunFunc();
 }
@@ -256,11 +256,151 @@ int __stdcall myWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCm
     //return originalWinMainFunc(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
 }
 
+///
+///
+/// DRAW MENU ITEMS
+/// 
+/// 
+
+__declspec(naked) void __fastcall thunk_FUN_005d0030(LPCRITICAL_SECTION param_1)
+{
+    OutputDebugStringA("DLL: thunk_FUN_005d0030");
+    __asm {
+        push 005d0030h
+        ret
+    }
+}
+
+__declspec(naked) void __fastcall thunk_FUN_006352f0(int param_1_00, HWND param_2, char* param_3, int param_4)
+{
+    //OutputDebugStringA("DLL: thunk_FUN_006352f0");
+    __asm {
+        push 006352f0h
+        ret
+    }
+}
+
+void __fastcall drawMenuItems(HINSTANCE* param_1_00, int param_2)
+{
+    char message[63];
+    sprintf_s(message, "%p", &param_1_00);
+    OutputDebugStringA("DLL: drawMenuItems() (possibly red herring)");
+    OutputDebugStringA(message);
+    //HWND pHVar1;
+    //FILE* pFVar2;
+    //int iVar3;
+    //long lVar4;
+    //char* pcVar5;
+    //BOOL unaff_EBP;
+    //CHAR local_64[100];
+
+    //if (param_2 == 0) {
+    //    LPCRITICAL_SECTION DAT_007c0004 = reinterpret_cast<LPCRITICAL_SECTION>(0x007c0004);
+    //    thunk_FUN_005d0030(DAT_007c0004);
+    //    HWND DAT_008fcc0c = reinterpret_cast<HWND>(0x008fcc0c); // hWndParent
+    //    thunk_FUN_006352f0((int)param_1_00, DAT_008fcc0c, (char*)"Graphics\\Menu\\Game Options.bmp", 0); // @ 0x006354A5
+    //    InvalidateRect(DAT_008fcc0c, (RECT*)0x0, 1);
+    //}
+    //if (DAT_007a45c8 != 0) {
+    //    thunk_FUN_00645ff0((int)param_1_00, &param_1_00[0x157]->unused, 10, 9, 0x1b2);
+    //}
+    //thunk_FUN_00644ce0(param_1_00, 10, 10, 0x80, 0x2d, &DAT_007b416c, 0xffffff, 0xffffffff, param_1_00[0x154])
+    //    ;
+    //pHVar1 = thunk_FUN_006441d0(param_1_00, 0xe0, 0x57, 0xc0, 0x18, s_New_Game_007b4174, 0, 0xe6e6e6, 0xdcdcdc
+    //    , 0xa0a0a, param_1_00[0x154], 0, 0xf);
+    //param_1_00[0x67c] = (HINSTANCE)pHVar1;
+    //pHVar1 = thunk_FUN_006441d0(param_1_00, 0xe0, 0x7f, 0xc0, 0x18, s_Continue_Previous_Game_007b4180, 0,
+    //    0xe6e6e6, 0xdcdcdc, 0xa0a0a, param_1_00[0x154], 0, 0xf);
+    //param_1_00[0x67d] = (HINSTANCE)pHVar1;
+    //pHVar1 = thunk_FUN_006441d0(param_1_00, 0xe0, 0xa7, 0xc0, 0x18, s_Load_Game_007b4198, 0, 0xe6e6e6,
+    //    0xdcdcdc, 0xa0a0a, param_1_00[0x154], 0, 0xf);
+    //param_1_00[0x67e] = (HINSTANCE)pHVar1;
+    //if (param_1_00[0x40b] == (HINSTANCE)0x0) {
+    //    EnableWindow((HWND)param_1_00[0x67d], 0);
+    //}
+    //if (param_1_00[0x40a] == (HINSTANCE)0x0) {
+    //    EnableWindow((HWND)param_1_00[0x67e], 0);
+    //}
+    //pHVar1 = thunk_FUN_006441d0(param_1_00, 0xe0, 0xcf, 0xc0, 0x18, s_Multiplayer_Game_007b41a4, 0, 0xe6e6e6,
+    //    0xdcdcdc, 0xa0a0a, param_1_00[0x154], 0, 0xf);
+    //param_1_00[0x67f] = (HINSTANCE)pHVar1;
+    //pHVar1 = thunk_FUN_006441d0(param_1_00, 0xe0, 0xf7, 0xc0, 0x18, s_Tutorial_007b41b8, 0, 0xe6e6e6, 0xdcdcdc
+    //    , 0xa0a0a, param_1_00[0x154], 0, 0xf);
+    //param_1_00[0x680] = (HINSTANCE)pHVar1;
+    //pHVar1 = thunk_FUN_006441d0(param_1_00, 0xe0, 0x11f, 0xc0, 0x18, s_Hall_of_Fame_007b41c4, 0, 0xe6e6e6,
+    //    0xdcdcdc, 0xa0a0a, param_1_00[0x154], 0, 0xf);
+    //param_1_00[0x681] = (HINSTANCE)pHVar1;
+    //pHVar1 = thunk_FUN_006441d0(param_1_00, 0xe0, 0x147, 0xc0, 0x18, s_Credits_007b41d4, 0, 0xe6e6e6, 0xdcdcdc
+    //    , 0xa0a0a, param_1_00[0x154], 0, 0xf);
+    //param_1_00[0x682] = (HINSTANCE)pHVar1;
+    //pHVar1 = thunk_FUN_006441d0(param_1_00, 0xe0, 0x16f, 0xc0, 0x18, &DAT_007b41dc, 0, 0xe6e6e6, 0xdcdcdc,
+    //    0xa0a0a, param_1_00[0x154], 0, 0xf);
+    //param_1_00[0x683] = (HINSTANCE)pHVar1;
+    //if (*(char*)(param_1_00 + 0x67b) == '\0') {
+    //    if (param_2 == 0) {
+    //        UpdateWindow(DAT_008fcc0c);
+    //        *(undefined*)(param_1_00 + 0x473) = 1;
+    //    }
+    //    pcVar5 = (char*)0x0;
+    //    pFVar2 = FID_conflict:__wfopen(s_graphics\interface\640\Lieutenan_007b42f0, &DAT_007b42ec);
+    //    iVar3 = FUN_0077d1f0((int)pFVar2);
+    //    lVar4 = __filelength(iVar3);
+    //    if (lVar4 != 0x5d394) {
+    //        pcVar5 = s_Sprite_files_have_not_been_updat_007b4324;
+    //    }
+    //    _fclose(pFVar2);
+    //    pFVar2 = FID_conflict:__wfopen(s_graphics\interface\800\Lieutenan_007b4370, &DAT_007b436c);
+    //    iVar3 = FUN_0077d1f0((int)pFVar2);
+    //    lVar4 = __filelength(iVar3);
+    //    if (lVar4 != 0x8ff1a) {
+    //        pcVar5 = s_Sprite_files_have_not_been_updat_007b43a4;
+    //    }
+    //    _fclose(pFVar2);
+    //    if (pcVar5 != (LPCSTR)0x0) {
+    //        thunk_FUN_005cf910(DAT_007c0004);
+    //        thunk_FUN_005cfd20(DAT_007c0004, (byte*)(param_1_00 + 0x14), 5);
+    //        *(undefined*)(param_1_00 + 0x473) = 0;
+    //        do {
+    //            iVar3 = ShowCursor(1);
+    //        } while (iVar3 < 0);
+    //        wsprintfA(local_64, pcVar5);
+    //        MessageBoxA(DAT_008fcc0c, local_64, s_ICS_Update_Error_007b43ec, 0);
+    //    }
+    //    param_1_00[0x279] = (HINSTANCE)0x1;
+    //    return;
+    //}
+    //do {
+    //    *(char*)&pHVar1->unused = *(char*)&pHVar1->unused + (char)pHVar1;
+    //    pHVar1 = (HWND)EnableWindow(pHVar1, unaff_EBP);
+    //    unaff_EBP = -0x70;
+    //} while (true);
+}
+
+///
+///
+/// DRAW MENU ITEMS ( MAYBE THIS ONE )
+/// 
+/// 
+// Original function signature
+typedef void(__thiscall* DrawMenuItemsFunc)(DWORD pThis, HWND param_1, char param_2);
+DrawMenuItemsFunc originalDrawMenuItemsFunc = nullptr;
+
+void __fastcall drawMenuItems2(DWORD pThis, HWND param_1, char param_2)
+{
+    OutputDebugStringA("DLL: MAYBE THE REAL drawMenuItems()");
+
+    //HINSTANCE* local_14 = (HINSTANCE*) 0x0019F9E0;
+    //thunk_FUN_006352f0((int)local_14, param_1, (char*)"Graphics\\Menu\\Main Menu.bmp", 1);
+
+    originalDrawMenuItemsFunc = reinterpret_cast<DrawMenuItemsFunc>(0x00634B20);
+    originalDrawMenuItemsFunc(pThis, param_1, param_2);
+    return;
+}
 
 void HookFunctions() {
-    writeJmp(0x00406195, myWinMain); // jmp WinMain
-    //writeJmp(0x00405966, local_sub_405966);
-    //writeJmp(0x004013B6, local_sub_4013B6);
+    writeJmp(0x00406195, myWinMain);
+    //writeJmp(0x00405763, drawMenuItems); 
+    writeJmp(0x00406CB2, drawMenuItems2);
     OutputDebugStringA("DLL: All functions hooked");
 
     //char buffer[100];
